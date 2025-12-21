47da367e5e6350b44655e933e42ec44f83eebba3
Flynn500 <zfp6323@aut.ac.nz>
2025-12-21 23:18:57 +1300

    0.1 Release
    0.1 version is live. Has the most bare bones features I was looking for so there you go!

--- src/dubious/__init__.py
+++ src/dubious/__init__.py
@@ -2,6 +2,8 @@ from .distributions import Normal, Distribution, LogNormal, Uniform, Beta
 from .uncertain import Uncertain, sample_uncertain
 from .uncertain import Context
 
+__version__ = "0.1.0"
+
 __all__ = ["Distribution", "Normal", "Uniform", "LogNormal", "Beta", #dists
            "Uncertain", "Context", 
            "sample_uncertain"]
