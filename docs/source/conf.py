20266979e6dcd3e3fb933374e11f0f170289c25d
Flynn500 <zfp6323@aut.ac.nz>
2025-12-27 00:52:06 +1300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../src"))
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, "..", "..", "..", "src")))
 
 project = 'dubious'
 copyright = '2025, Flynn Wilson'
